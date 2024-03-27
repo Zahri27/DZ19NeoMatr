2415a3e0a74cf3c03f75284646a076b5a535d182
Zahri27 <privetmurik@gmail.com>
2024-03-27 12:49:16 +0200

    innit

---
+++ src/TrainingMatrix.java
@@ -0,0 +1,23 @@
+public class TrainingMatrix {
+
+    public static void main(String[] args) {
+        int[][] matrix = new int[8][8];
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (i % 2 == 0) {
+                    matrix[i][j] = j + 1;
+                } else {
+                    matrix[i][j] = matrix[i].length - j;
+                }
+            }
+        }
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
